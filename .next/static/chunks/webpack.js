053523ec956e39ad5dc7cf6c5f133ca93a7c824e
KurutoDenzeru <kurtdenzel51@gmail.com>
2024-05-20 00:17:37 +0800

    feat: .next files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "840a99c660399f57"; }
+/******/ 		__webpack_require__.h = function() { return "a4b8f3f0468c7e2d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
